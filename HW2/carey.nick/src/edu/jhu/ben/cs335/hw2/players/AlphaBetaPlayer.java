02497c492b98d13c316c6120aa1dd0e3258824ea
ncarey <ncarey90@gmail.com>
2014-02-28 14:26:23 -0500

    Initial HW2 commit

---
+++ HW2/carey.nick/src/edu/jhu/ben/cs335/hw2/players/AlphaBetaPlayer.java
@@ -0,0 +1,22 @@
+package edu.jhu.ben.cs335.hw2.players;
+
+import edu.jhu.ben.cs335.hw2.players.Player;
+import edu.jhu.ben.cs335.hw2.board.Board;
+import edu.jhu.ben.cs335.hw2.board.Move;
+
+/** alpha-beta pruning Player class for Konane game
+ *
+ * @author Ben Mitchell
+ */
+public class AlphaBetaPlayer extends Player {
+
+  public Move getMove(Board game) {
+    Move ret = new Move(-1, -1, -1, -1);
+
+    //TODO
+
+    return ret;
+  }
+
+
+}
